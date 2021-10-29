159843ca30cf68953a24faeec200f33cf7a4ca54
송찬흠 <bsk9212@gmail.com>
2021-10-29 18:51:43 +0900

    기본 small로 수정

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,11 +1,12 @@
 import { Divider, Flex, IconButton } from '@chakra-ui/react'
 import {FiMenu, FiHome, FiBook, FiChevronsRight, FiSettings} from 'react-icons/fi';
+import {BiBaseball} from 'react-icons/bi';
 import React, {useState} from 'react'
 import NavItem from './NavItem';
 import { useSelector } from 'react-redux';
 
 function Sidebar() {
-    const [navSize, changeNavSize] = useState("large")
+    const [navSize, changeNavSize] = useState("small")
     
     const categories = useSelector(state => state.category);
     return (
@@ -40,7 +41,8 @@ function Sidebar() {
                 <NavItem navSize={navSize} icon={FiHome} title="대시보드" path="dashboard"/>
                 <NavItem navSize={navSize} icon={FiBook} title="전체보기" path="posts"/>
                 <NavItem navSize={navSize} icon={FiChevronsRight} title="카테고리별" description={categories.category}/>
-                <NavItem navSize={navSize} icon={FiSettings} title="setting" path="setting"/>
+                <NavItem navSize={navSize} icon={BiBaseball} title="야구" path="baseball" />
+                <NavItem navSize={navSize} icon={FiSettings} title="설정" path="setting"/>
             </Flex>
         </Flex>
     )
